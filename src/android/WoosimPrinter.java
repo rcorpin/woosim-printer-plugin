9d194ccbc37daca600f496dbb5cfef6fff597de6
Rizielle Corpin <rcorpin@wyzelink.com>
2014-12-11 14:38:49 -0500

    fixing Arguments to path.join must be strings

--- src/android/WoosimPrinter.java
+++ src/android/WoosimPrinter.java
@@ -7,18 +7,14 @@ import org.json.JSONObject;
 import org.json.JSONArray;
 import org.json.JSONException;
 
-import com.woosim.bt.WoosimPrinter;
 
 public class WoosimPrinter extends CordovaPlugin {
 	public static final String ACTION_PRINT_TEST = "printTest";
-	public WoosimPrinter woosim;
 	
 	@Override
 	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 		try {
 			if (ACTION_PRINT_TEST.equals(action)) {
-				woosim.saveSpool(EUC_KR, "Cordova Plugin Test", 0, false);
-				woosim.printSpool(true);
 				callbackContext.success();
 	            return true;
 			}
