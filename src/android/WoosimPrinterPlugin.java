8c31ef51c5001f0313e3a474a88877de75523a25
Rizielle Corpin <rcorpin@wyzelink.com>
2014-12-11 22:42:46 -0500

    adding printer check connection

--- src/android/WoosimPrinterPlugin.java
+++ src/android/WoosimPrinterPlugin.java
@@ -42,7 +42,7 @@ public class WoosimPrinterPlugin extends CordovaPlugin {
 				callbackContext.success(ret);
 				return true;
 			} else if (ACTION_PRINT_TEST.equals(action)) {
-				woosim.saveSpool(EUC_KR, " Sales Receipt\r\n\r\n\r\n", 0x11, true);
+				woosim.saveSpool(EUC_KR, " Sales Invoice\r\n\r\n\r\n", 0x11, true);
 				woosim.saveSpool(EUC_KR, "MERCHANT NAME                               Dr. Green\r\n", 0, false);
 				int ret = woosim.printSpool(true);
 				callbackContext.success(ret);
